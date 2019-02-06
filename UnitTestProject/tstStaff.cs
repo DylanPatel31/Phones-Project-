197a4f62a86a78e4b05a9f2d1a9c70459da2881c
DylanPatel31 <p16199904@my365.dmu.ac.uk>
2019-02-06 11:50:17 +0000

    new test and class for staff

---
+++ UnitTestProject/tstStaff.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestProject
+{
+    [TestClass]
+    public class tstStaff
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+
+            //create an instance of our class clsStaff
+            clsStaff AStaff = new clsStaff();
+            //check to see that the class in not null
+            Assert.IsNotNull(AStaff);
+        }
+    }
+}
