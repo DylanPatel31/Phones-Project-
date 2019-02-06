f1a5adfd28cbd9f868da4c6a00791bc5a52e3a7c
p16209753 <p16209753@GWHXX653WIN014W>
2019-02-06 13:08:01 +0000

    Created tstStock create clsStock

---
+++ ClassLibrary/clsStock.cs
@@ -0,0 +1,79 @@
+﻿namespace ClassLibrary
+{
+    public class clsStock
+    {
+
+        private string phoneMake;
+        public string PhoneMake
+
+        {
+
+            get
+            {
+                return phoneMake;
+            }
+                       
+            set
+            {
+                phoneMake = value;
+            }
+
+
+
+        }
+        private string phoneModel;
+        public string PhoneModel
+        {
+
+
+              get
+            {
+                return phoneModel;
+            }
+                
+                
+              set
+            {
+                phoneModel = value; 
+            }
+                
+                
+                
+         }
+        private int phoneID;
+        public int PhoneID
+        {
+
+
+            get
+            {
+                return phoneID;
+
+            }
+            
+            
+            set
+            {
+                phoneID = value; 
+
+            }
+                
+        }
+        private string phoneDescription;
+        public string PhoneDescription
+         {
+            get
+            {
+              return phoneDescription;
+            }
+               
+                
+            set
+            {
+                phoneDescription = value;
+            }
+                
+                
+        }
+    }
+}
