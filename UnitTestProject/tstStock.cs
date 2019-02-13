2f651e5f571de56db7c9be7b5a07212d67e6dd96
p16209753 <p16209753@GWHXX653WIN014W>
2019-02-13 11:25:14 +0000

    some vchanges

---
+++ UnitTestProject/tstStock.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using ClassLibrary;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestProject
+{
+    [TestClass]
+    public class tstStock
+    {
+        [TestMethod]
+        public void InstanceOK()
+        {   //create an instance of the class
+            clsStock AnStock = new clsStock();
+            //test to see if it exists 
+            Assert.IsNotNull(AnStock);
+        }
+
+
+        [TestMethod]
+        public void PhoneMakeExists()
+        {
+            //create some test data
+            clsStock AnStock = new clsStock();
+            //create some test data 
+            string Test = "Iphone";
+            //assign data to property
+            AnStock.PhoneMake = Test;
+            //test to see if values are the same 
+            Assert.AreEqual(AnStock.PhoneMake, Test);
+
+        }
+
+        [TestMethod]
+        public void PhoneModelExists()
+        {
+            //create some test data
+            clsStock AnStock = new clsStock();
+            //create some test data 
+            string Test = "XSMAX";
+            //assign data to property
+            AnStock.PhoneModel = Test;
+            //test to see if values are the same 
+            Assert.AreEqual(AnStock.PhoneModel, Test);
+
+        }
+
+
+        [TestMethod]
+        public void PhoneIDExists()
+        {
+            //create some test data
+            clsStock AnStock = new clsStock();
+            //create some test data 
+            Int32 Test = 1;
+            //assign data to property
+            AnStock.PhoneID = Test;
+            //test to see if values are the same 
+            Assert.AreEqual(AnStock.PhoneID, Test);
+
+        }
+
+
+        [TestMethod]
+        public void PhoneDescriptionExists()
+        {
+            //create some test data
+            clsStock AnStock = new clsStock();
+            //create some test data 
+            string Test = "256gb";
+            //assign data to property
+            AnStock.PhoneDescription = Test;
+            //test to see if values are the same 
+            Assert.AreEqual(AnStock.PhoneDescription, Test);
+
+        }
+
+
+
+
+
+
+
+
+
+    }
+}
