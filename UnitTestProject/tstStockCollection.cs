6b2a56d055df81aa827e1e6706ad514d40af9338
p16209753 <p16209753@GWHXX653WIN014W>
2019-03-06 12:16:57 +0000

    ee

--- UnitTestProject/tstStockCollection.cs
+++ UnitTestProject/tstStockCollection.cs
@@ -17,12 +17,8 @@ namespace UnitTestProject
             Assert.IsNotNull(AllStock);
         }
 
-
-
-
-
         [TestMethod]
-        
+
         public void StockListOk()
         {
             //create an instance of the class we want to create 
@@ -46,15 +42,53 @@ namespace UnitTestProject
             Assert.AreEqual(AllStock.StockList, TestList);
 
         }
-        
+
+
 
         [TestMethod]
 
-        public void 
+        public void CountyPropertyOk()
+
+        {
+            //create an instance of the class we want to create 
+            clsStockCollection AllStock = new clsStockCollection();
+            //create some test data to assign to the property 
+            Int32 SomeCount = 0;
+            //assign the data to the property
+            AllStock.Count = SomeCount;
+            //test to see theat two values are the same 
+            Assert.AreEqual(AllStock.Count, SomeCount);
+
+        }
 
 
+        [TestMethod]
+
+        public void ThisStockPropertyOk()
+
+        {
+            //create an instance of the class
+            clsStockCollection AllStock = new clsStockCollection();
+            //create some test data to assign the property 
+            clsStock TestStock = new clsStock();
+            //set the properties of the test object 
+            TestStock.PhoneID = 1;
+            TestStock.PhoneMake = "sa";
+            TestStock.PhoneModel = "w";
+            TestStock.PhoneDescription = "s";
+            //assign the data to the property 
+            AllStock.ThisStock = TestStock;
+            //test to see that the two values are the same 
+            Assert.AreEqual(AllStock.ThisStock, TestStock);
+
+        }
+
+
+        [TestMethod]
+        
 
 
     }
+
 
 }
