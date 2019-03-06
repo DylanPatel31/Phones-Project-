74e1dbe9f337988ac47610cf43035625fdddf6fb
p16209753 <p16209753@GWHXX653WIN014W>
2019-03-06 10:49:12 +0000

    tststockcollection

---
+++ UnitTestProject/tstStockCollection.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using ClassLibrary;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+
+namespace UnitTestProject
+{
+    [TestClass]
+    public class tstStockCollection
+    {
+        [TestMethod]
+        public void InstanceOK()
+        {
+            //create an instance of the class we want to create
+            clsStockCollection AllStock = new clsStockCollection();
+            //test to see that it exists
+            Assert.IsNotNull(AllStock);
+        }
+
+
+
+
+
+        [TestMethod]
+        
+        public void StockListOk()
+        {
+            //create an instance of the class we want to create 
+            clsStockCollection AllStock = new clsStockCollection();
+            //create some test data to assign to the property 
+            //in this case the data needs to be a list of objects 
+            List<clsStock> TestList = new List<clsStock>();
+            //add an item to the list 
+            //create the item of the test
+            clsStock TestItem = new clsStock();
+            //set its properties 
+            TestItem.PhoneID = 1;
+            TestItem.PhoneMake = "s";
+            TestItem.PhoneModel = "s";
+            TestItem.PhoneDescription = "s";
+            //add the item to the test list 
+            TestList.Add(TestItem);
+            //assign the data to the property 
+            AllStock.StockList = TestList;
+            //test to see that the two values are the same 
+            Assert.AreEqual(AllStock.StockList, TestList);
+
+        }
+        
+
+        [TestMethod]
+
+        public void 
+
+
+
+
+    }
+
+}
