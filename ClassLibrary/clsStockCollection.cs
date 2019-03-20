bdb42e9454a4ad41e841d39e8839852991f8f3b2
p16209753 <p16209753@GWHXX653WIN014W>
2019-03-20 14:45:31 +0000

    update

--- ClassLibrary/clsStockCollection.cs
+++ ClassLibrary/clsStockCollection.cs
@@ -22,8 +22,8 @@ namespace ClassLibrary
             {
                 mStockList = value;
             }
-                
-         }
+
+        }
         public int Count
         {
             get
@@ -62,46 +62,59 @@ namespace ClassLibrary
             throw new NotImplementedException();
         }
 
+        public void Delete()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ReportByMake(string v)
+        {
+            throw new NotImplementedException();
+        }
+
+        
         //constructor for the class
-        //  public clsStaffCollection()
-        // {
-        //var for the index
-        //  Int32 Index = 0;
+        //public clsStaffCollection()
+        //{
+        //    //var for the index
+        //    Int32 Index = 0;
 
-        //var to store the record count 
-        //Int32 RecordCount = 0;
+        //    //var to store the record count 
+        //    Int32 RecordCount = 0;
 
-        //object for data connection 
-        // clsDataConnection DB = new clsDataConnection();
-        //execute the stored procedure 
-        // DB.Execute("sproc_tblStock_SelectAll");
-        //get count of records 
-        // RecordCount = DB.Count;
-        //while ther are records to process
-        // while (Index < RecordCount)
-        // {
-        //create a blank address
-        // clsStock AnStock = new clsStock();
-        //read in the fields from the current record 
-        // AnStock.PhoneID = Convert.ToInt32(DB.DataTable.Rows[Index]["PhoneID"]);
-        // AnStock.PhoneMake = Convert.ToString(DB.DataTable.Rows[Index]["PhoneMake"]);
-        // AnStock.PhoneModel = Convert.ToString(DB.DataTable.Rows[Index]["PhoneModel"]);
-        // AnStock.PhoneDescription = Convert.ToString(DB.DataTable.Rows[Index]["PhoneDescription"]);
-        // add the record to the private data member 
-        // mStockList.Add(AnStock);
-        //point at the next record 
-        // Index++;
+        //    //object for data connection 
+        //    clsDataConnection DB = new clsDataConnection();
+        //    //execute the stored procedure 
+        //    DB.Execute("sproc_tblStock_SelectAll");
+        //    //get count of records 
+        //    RecordCount = DB.Count;
+        //    //while ther are records to process
+        //    while (Index < RecordCount)
+        //    {
+        //        //create a blank address
+        //        clsStock AnStock = new clsStock();
+        //        //read in the fields from the current record 
+        //        AnStock.PhoneID = Convert.ToInt32(DB.DataTable.Rows[Index]["PhoneID"]);
+        //        AnStock.PhoneMake = Convert.ToString(DB.DataTable.Rows[Index]["PhoneMake"]);
+        //        AnStock.PhoneModel = Convert.ToString(DB.DataTable.Rows[Index]["PhoneModel"]);
+        //        AnStock.PhoneDescription = Convert.ToString(DB.DataTable.Rows[Index]["PhoneDescription"]);
+        //        // add the record to the private data member 
+        //        mStockList.Add(AnStock);
+        //        //point at the next record 
+        //        Index++;
+
+        //    }
 
         //}
 
 
 
 
-    }
-        
-
-
-
 
 
 
@@ -111,7 +124,7 @@ namespace ClassLibrary
 
     }
 
-
+}
    
 
 
--- UnitTestProject/tstStockCollection.cs
+++ UnitTestProject/tstStockCollection.cs
@@ -91,33 +91,170 @@ namespace UnitTestProject
         }
         
 
-       [TestMethod]
+       //[TestMethod]
 
-       public void AddMethodOk()
-        {
-            //create an instance of the class we want to create
-            clsStockCollection AllStock = new clsStockCollection();
-            //create the item of test data
-            clsStock TestItem = new clsStock();
-            //var to store the primary key 
-            Int32 PrimaryKey = 0;
-            //set its properties 
-            TestItem.PhoneID = 1;
-            TestItem.PhoneMake = "Iphone";
-            TestItem.PhoneModel = "XS";
-            TestItem.PhoneDescription = "256GB";
-            //set thisStock to the test data
-            AllStock.ThisStock = TestItem;
-            //add the record
-            PrimaryKey = AllStock.Add();
-            //set the primary key of the test data
-            TestItem.PhoneID = PrimaryKey;
-            //find the record 
-            AllStock.ThisStock.Find(PrimaryKey);
-            //test to see that the two values are the same 
-            Assert.AreEqual(AllStock.ThisStock, TestItem);
+       //public void AddMethodOk()
+       // {
+       //     //create an instance of the class we want to create
+       //     clsStockCollection AllStock = new clsStockCollection();
+       //     //create the item of test data
+       //     clsStock TestItem = new clsStock();
+       //     //var to store the primary key 
+       //     Int32 PrimaryKey = 0;
+       //     //set its properties 
+       //     TestItem.PhoneID = 1;
+       //     TestItem.PhoneMake = "Iphone";
+       //     TestItem.PhoneModel = "XS";
+       //     TestItem.PhoneDescription = "256GB";
+       //     //set thisStock to the test data
+       //     AllStock.ThisStock = TestItem;
+       //     //add the record
+       //     PrimaryKey = AllStock.Add();
+       //     //set the primary key of the test data
+       //     TestItem.PhoneID = PrimaryKey;
+       //     //find the record 
+       //     AllStock.ThisStock.Find(PrimaryKey);
+       //     //test to see that the two values are the same 
+       //     Assert.AreEqual(AllStock.ThisStock, TestItem);
 
-        }
+       // }
+
+
+
+
+        //[TestMethod]
+        //public void DeleteMethodOK()
+        //{
+        //    //create an instance of the class we want to create
+        //    clsStockCollection AllStock = new clsStockCollection();
+        //    //create the item of test data 
+        //    clsStock TestItem = new clsStock();
+        //    //var to store the primary key
+        //   Int32 PrimaryKey = 0;
+        //    //set the properties 
+        //    //set its properties 
+        //    TestItem.PhoneID = 1;
+        //    TestItem.PhoneMake = "Iphone";
+        //    TestItem.PhoneModel = "XS";
+        //    TestItem.PhoneDescription = "256GB";
+        //    //set thisStock to the test data
+        //    AllStock.ThisStock = TestItem;
+        //    //add the record
+        //    PrimaryKey = AllStock.Add();
+        //    //set the primary key of the test data
+        //    TestItem.PhoneID = PrimaryKey;
+        //    //find the record 
+        //    AllStock.ThisStock.Find(PrimaryKey);
+        //    //delete the record 
+        //    AllStock.Delete();
+        //    //find the record 
+        //    Boolean Found = AllStock.ThisStock.Find(PrimaryKey);
+        //    //test to see that the record was not found 
+        //    Assert.IsFalse(Found);
+
+        //}
+
+        //[TestMethod]
+
+        //public void UpdaeMethodOK()
+        //{
+        //    //create an instance of the class we want to create
+        //    clsStockCollection AllStock = new clsStockCollection();
+        //    //create the item of test data 
+        //    clsStock TestItem = new clsStock();
+        //    //var to store the primary key
+        //    Int32 PrimaryKey = 0;
+        //    //set the properties  
+        //    TestItem.PhoneID = 1;
+        //    TestItem.PhoneMake = "Iphone";
+        //    TestItem.PhoneModel = "XS";
+        //    TestItem.PhoneDescription = "256GB";
+        //    //set thisStock to the test data
+        //    AllStock.ThisStock = TestItem;
+        //    //add the record
+        //    PrimaryKey = AllStock.Add();
+        //    //set the primary key of the test data
+        //    TestItem.PhoneID = PrimaryKey;
+        //    //modify the test data 
+        //    TestItem.PhoneID = 2;
+        //    TestItem.PhoneMake = "Iphon";
+        //    TestItem.PhoneModel = "XSMax";
+        //    TestItem.PhoneDescription = "512GB";
+        //    //set the record based on the new test data 
+        //    AllStock.ThisStock = TestItem;
+        //    //update the record 
+        //    AllStock.Update();
+        //    //find the record 
+        //    AllStock.ThisStock.Find(PrimaryKey);
+        //    //test to see thisStockk matches the test data
+        //    Assert.AreEqual(AllStock.ThisStock, TestItem);
+
+        //}
+
+        //[TestMethod]
+
+        //public void ReportByPhoneMakeMethodOk()
+        //{
+        //    //create an instance of the class containing unfiltered results 
+        //    clsStockCollection AllStock = new clsStockCollection();
+        //    //create an instace of the filtered data
+        //    clsStockCollection FilteredStock = new clsStockCollection();
+        //    //apply a blank string (Should return all records);
+        //    FilteredStock.ReportByMake("");
+        //    //test to see that the two values are the same 
+        //    Assert.AreEqual(AllStock.Count, FilteredStock.Count);
+        //}
+
+
+        //[TestMethod]
+
+        //public void ReportByPhoneMakeNoneFound()
+        //{
+        //    //create an instace of the filtered data
+        //    clsStockCollection FilteredStock = new clsStockCollection();
+        //    //apply a blank string (Should return all records);
+        //    FilteredStock.ReportByMake("xxx");
+        //    //test to see that the two values are the same 
+        //    Assert.AreEqual(0, FilteredStock.Count);
+        //}
+
+
+        //[TestMethod]
+
+        //public void ReportByPhoneMakeTestDataFound()
+        //{
+        //    //create an instace of the filtered data 
+        //    clsStockCollection FilteredStock = new clsStockCollection();
+        //    //var to store the outcome 
+        //    Boolean OK = true;
+        //    //apply a phone make that doesnt exist 
+        //    FilteredStock.ReportByMake("Iphone");
+        //    //check that the correct number of records are found 
+        //    if (FilteredStock.Count == 2)
+        //    {
+        //        //check that the first record is ID 1
+        //        if (FilteredStock.StockList[0].PhoneID != 1)
+        //        {
+        //            OK = false;
+
+        //        }
+        //        //check to see that first record is 10
+        //        if (FilteredStock.StockList[1].PhoneID != 10)
+        //        {
+        //            OK = false;
+        //        }
+        //    }
+        //    else
+        //    {
+        //        OK = false;
+        //    }
+        //    //test to see that there are no records
+        //    Assert.IsTrue(OK);
+        //}
+
+
+
+
 
 
 
