35031b4df71df21bd3c2071e546cfe8808ef75d7
Dylan Patel <46681645+DylanPatel31@users.noreply.github.com>
2019-03-22 08:09:14 +0000

    new clsStaffCollection

--- ClassLibrary/clsStaffCollection.cs
+++ ClassLibrary/clsStaffCollection.cs
@@ -7,7 +7,10 @@ namespace ClassLibrary
     {
         //private data member for the list
         List<clsStaff> mStaffList = new List<clsStaff>();
-
+        //private dta member thisStaff
+        clsStaff mThisStaff = new clsStaff();
+        //private data member to connect to the database
+        private clsDataConnection DB = new clsDataConnection();
 
         //constructor for the class
         public clsStaffCollection()
@@ -46,6 +49,7 @@ namespace ClassLibrary
 
         }
 
+
         //public property for the staff list
         public List<clsStaff> StaffList
         {
@@ -75,10 +79,42 @@ namespace ClassLibrary
             }
         }
 
+        //public property for ThisStaff
+        public clsStaff ThisStaff
+        {
+            get
+            {
+                //return the private data
+                return mThisStaff;
+            }
+            set
+            {
+                //set the private data
+                mThisStaff = value;
+            }
+        }
+
+       
+
+        public int Add()
+        {
+            //adds a new record to the database based on the values of mThisStaff
+            //connect to the database
+            clsDataConnection DB = new clsDataConnection();
+            //set the parameters for the stored procedure
+            DB.AddParameter("@Title", mThisStaff.Title);
+            DB.AddParameter("@FirstName", mThisStaff.FirstName);
+            DB.AddParameter("@LastName", mThisStaff.LastName);
+            DB.AddParameter("@DateOfBirth", mThisStaff.DateOfBirth);
+            DB.AddParameter("@Address1", mThisStaff.Address1);
+            DB.AddParameter("@Address2", mThisStaff.Address2);
+            DB.AddParameter("@City", mThisStaff.City);
+            DB.AddParameter("@PostCode", mThisStaff.PostCode);
+            //execute the query returning the primary key value
+            return DB.Execute("sproc_tblStaff_Insert");
+        }
 
 
-        
-        public clsStaff ThisStaff { get; set; }
     }
 
     
